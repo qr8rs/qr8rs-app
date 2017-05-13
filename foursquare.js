6f235d1d5a0ad0725467e74ec2f35e211b4df1a8
Chuck-Hou Yee <chuckyee@physics.rutgers.edu>
2017-05-13 19:10:51 -0400

    Use dotenv for Foursquare credentials.

--- foursquare.js
+++ foursquare.js
@@ -1,5 +1,8 @@
-// var foursquare = (require('foursquarevenues'))('CLIENTIDKEY', 'CLIENTSECRETKEY');
-var foursquare = (require('foursquarevenues'))('Y5D1QX0FS4R5ZTJ5TLU3RDL1U0PUE4ZU3NPTQDS2AGMW1KGP', 'XM2PZ15QKM0I5TQYTCTYXOVBGWKRHCCFZFNNKZYQA1JV1SNZ');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+var foursquare = (require('foursquarevenues'))(process.env.FOURSQUARE_CLIENT_ID, process.env.FOURSQUARE_CLIENT_SECRET);
 
 var params = {
     "ll": "40.7,-74"
@@ -7,12 +10,12 @@ var params = {
 
 foursquare.getVenues(params, function(error, venues) {
     if (!error) {
-	console.log(venues.toString());
+	console.log(venues);
     }
 });
 
 foursquare.exploreVenues(params, function(error, venues) {
     if (!error) {
-  	console.log(venues.toString());
+  	console.log(venues);
     }
 });
